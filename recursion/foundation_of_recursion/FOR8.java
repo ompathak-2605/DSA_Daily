e2efc36650f6d7fd494f6b699fd23d50a5148e34
Om <userpc6996@gmail.com>
2026-05-22 23:56:35 +0530

    recursion ongoing

--- recursion/foundation_of_recursion/FOR8.java
+++ recursion/foundation_of_recursion/FOR8.java
@@ -1,4 +1,4 @@
-package recursion.foundation_of_recursion;
+
 // Find nth Fibonacci number recursively. 
 public class FOR8 {
     public static void main(String[] args) {
@@ -8,7 +8,7 @@ public class FOR8 {
     static int fibo(int n){
         if(n<=0) return 0;
         if(n==1) return 1;
-        
+        return fibo(n-1) + fibo(n-2); 
         
     }
 }
---
+++ recursion/foundation_of_recursion/FOR9.java
@@ -0,0 +1,13 @@
+// Print Fibonacci series up to n terms recursively. 
+public class FOR9 {
+    public static void main(String[] args) {
+        int n = 10;
+        fibo(n,0,1);
+    }
+    static void fibo(int n,int a, int b){
+        if(n==0) return ;
+        System.out.print(a+" ");
+        fibo(n-1,b,a+b);
+        
+    }
+}

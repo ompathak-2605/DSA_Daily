2a9b94b28878ab900c8cd53e88235b8fbc823ce9
Om <userpc6996@gmail.com>
2026-05-25 23:57:22 +0530

    in progress

---
+++ recursion/number_based_recursion/NRT2.java
@@ -0,0 +1,11 @@
+// Reverse a number recursively.
+public class NRT2 {
+    public static void main(String[] args) {
+        int num = 12345;
+        System.out.println(reverse(0,num));
+    }
+    static int reverse(int a ,int n){
+        if(n==0) return 1;
+        int r = a*10+()
+    }
+}
